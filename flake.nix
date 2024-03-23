3d9bc788244680b6b5bd9018e4bfeb9a96fb29e6
benluelo <git@luelo.dev>
2024-03-22 20:48:22 -0400

    feat(mac): set bash PS1

--- flake.nix
+++ flake.nix
@@ -93,7 +93,27 @@
         nix.settings.experimental-features = "nix-command flakes";
 
         # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.bash.enable = true; # default shell on catalina
+        programs.bash = {
+          # default shell on catalina
+          enable = true;
+          # copied from https://github.com/NixOS/nixpkgs/blob/56528ee42526794d413d6f244648aaee4a7b56c0/nixos/modules/programs/bash/bash.nix#L78
+          interactiveShellInit = ''
+            # Provide a nice prompt if the terminal supports it.
+            if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+              PROMPT_COLOR="1;31m"
+              ((UID)) && PROMPT_COLOR="1;32m"
+              if [ -n "$INSIDE_EMACS" ]; then
+                # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+                PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+              else
+                PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+              fi
+              if test "$TERM" = "xterm"; then
+                PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+              fi
+            fi
+          '';
+        };
         # programs.fish.enable = true;
 
         # Set Git commit hash for darwin-version.
